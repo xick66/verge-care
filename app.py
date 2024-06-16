34ffde3376aa51e108604a7a012eb886cf4755b4
Rahul K M <126589920+xick66@users.noreply.github.com>
2024-06-16 23:43:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -240,7 +240,7 @@ def main():
   width: 230px;
   height: 310px;
   border-radius: 10px;
-  background: linear-gradient(-45deg, #e81cff 0%, #40c9ff 100%);
+  background: linear-gradient(45deg, #fdf497 0%, #fdf497 25%, #fd5949 50%, #d6249f 75%, #285AEB 100%);
   z-index: -10;
   pointer-events: none;
   transition: all 0.6s cubic-bezier(0.175, 0.885, 0.32, 1.275);
@@ -251,7 +251,7 @@ def main():
   z-index: -1;
   position: absolute;
   inset: 0;
-  background: linear-gradient(-45deg, #fc00ff 0%, #00dbde 100%);
+  background: linear-gradient(45deg, #fdf497 0%, #fdf497 25%, #fd5949 50%, #d6249f 75%, #285AEB 100%);
   transform: translate3d(0, 0, 0) scale(0.95);
   filter: blur(20px);
   transition: filter 0.3s ease, background 0.3s ease;
