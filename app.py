206a3fc1954a2441b4b29bee3f68f8b9fbd9128a
Rahul K M <126589920+xick66@users.noreply.github.com>
2024-06-17 12:50:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -72,6 +72,15 @@ def main():
         menu_items={}
     )
 
+    hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
     # Custom CSS for styling
     st.markdown("""
         <style>
